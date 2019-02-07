a7f1f4a0d7d54117ba664e45f7f5659452986b33
Maximilian Algehed <m.algehed@gmail.com>
2019-02-07 10:48:39 +0100

    Refactor (polymorphism): Broke binary operation shrinking into polymorphic binOpShrink

--- test/deps/ProgramRepTests.hs
+++ test/deps/ProgramRepTests.hs
@@ -1,10 +1,17 @@
-{-# LANGUAGE DataKinds, FlexibleInstances #-}
+{-# LANGUAGE DataKinds, FlexibleInstances, FlexibleContexts #-}
 module ProgramRepTests where
 
 import Test.QuickCheck
 import Data.Maybe
 
 import ProgramRep
+
+binOpShrink :: Arbitrary a => (a -> a -> a) -> a -> a -> [a]
+binOpShrink op a0 a1 =  [a0, a1]
+                     ++ shrink a0
+                     ++ shrink a1
+                     ++ [ a `op` a1 | a <- shrink a0 ]
+                     ++ [ a0 `op` a | a <- shrink a1 ]
 
 instance Arbitrary Variable where
   arbitrary = Name . (:[]) <$> elements "abcdefghijklmnopqrstuvxyz"
@@ -20,10 +27,7 @@ instance Arbitrary Expr where
   shrink e = case e of
     Var _     -> []
     Lit i     -> Lit <$> shrink i
-    e0 :+: e1 -> shrink e0 
-              ++ shrink e1
-              ++ ((:+: e1) <$> shrink e0)
-              ++ ((e0 :+:) <$> shrink e1)
+    e0 :+: e1 -> binOpShrink (:+:) e0 e1
 
 instance Arbitrary Condition where
   arbitrary = sized go
@@ -41,8 +45,7 @@ instance Arbitrary Condition where
     e0 :==: e1 -> ((e0 :==:) <$> shrink e1)
                ++ ((:==: e1) <$> shrink e0)
 
-    e0 :&&: e1 -> ((e0 :&&:) <$> shrink e1)
-               ++ ((:&&: e1) <$> shrink e0)
+    c0 :&&: c1 -> binOpShrink (:&&:) c0 c1
     
     CNot c     -> CNot <$> shrink c
 
@@ -57,18 +60,11 @@ instance Arbitrary AtomicStatement where
     WriteArray v e0 e1 -> (WriteArray v e0 <$> shrink e1)
                        ++ ((\e -> WriteArray v e e1) <$> shrink e0)
 
-shrinkStmt :: Statement h -> [Statement h]
+shrinkStmt :: Arbitrary (Statement h) => Statement h -> [Statement h]
 shrinkStmt s = case s of
   SAtom a      -> SAtom <$> shrink a
-  SSeq s0 s1   -> [s0, s1]
-               ++ shrinkStmt s0
-               ++ shrinkStmt s1
-               ++ ((SSeq s0)         <$> shrinkStmt s1)
-               ++ ((\s -> SSeq s s1) <$> shrinkStmt s0)
-  SIf c s0 s1  -> [s0, s1]
-               ++ ((\c -> SIf c s0 s1) <$> shrink c)
-               ++ ((\s -> SIf c s s1) <$> shrinkStmt s0)
-               ++ ((\s -> SIf c s0 s) <$> shrinkStmt s1)
+  SSeq s0 s1   -> binOpShrink SSeq s0 s1 
+  SIf c s0 s1  -> binOpShrink (SIf c) s0 s1 ++ [ SIf c' s0 s1 | c' <- shrink c ] 
   SWhile c s   -> [s]
                ++ ((\c -> SWhile c s) <$> shrink c)
                ++ ((SWhile c) <$> shrinkStmt s)
@@ -82,6 +78,7 @@ instance Arbitrary (Statement NoHole) where
                    , SSeq <$> go (d `div` 2) <*> go (d `div` 2)
                    , SIf  <$> arbitrary <*> go (d `div` 2) <*> go (d `div` 2)
                    , SWhile <$> arbitrary <*> go (d `div` 2) ]
+
   shrink = shrinkStmt
 
 instance Arbitrary (Statement MaybeHole) where
@@ -94,6 +91,7 @@ instance Arbitrary (Statement MaybeHole) where
                    , SSeq <$> go (d `div` 2) <*> go (d `div` 2)
                    , SIf  <$> arbitrary <*> go (d `div` 2) <*> go (d `div` 2)
                    , SWhile <$> arbitrary <*> go (d `div` 2) ]
+
   shrink = shrinkStmt
 
 prop_id :: Statement NoHole -> Edit -> Bool
