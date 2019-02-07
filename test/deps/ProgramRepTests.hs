b67c39849211f3aac0f8dd8a5527e5c59136683d
Maximilian Algehed <m.algehed@gmail.com>
2019-02-07 10:17:02 +0100

    Test: Added shrinks to all the arbitrary instances

--- test/deps/ProgramRepTests.hs
+++ test/deps/ProgramRepTests.hs
@@ -17,6 +17,14 @@ instance Arbitrary Expr where
         d -> oneof [ Var <$> arbitrary , Lit <$> arbitrary
                    , (:+:) <$> go (d `div` 2) <*> go (d `div` 2) ]
 
+  shrink e = case e of
+    Var _     -> []
+    Lit i     -> Lit <$> shrink i
+    e0 :+: e1 -> shrink e0 
+              ++ shrink e1
+              ++ ((:+: e1) <$> shrink e0)
+              ++ ((e0 :+:) <$> shrink e1)
+
 instance Arbitrary Condition where
   arbitrary = sized go
     where
@@ -26,10 +34,44 @@ instance Arbitrary Condition where
                    , (:&&:) <$> go (d `div` 2) <*> go (d `div` 2)
                    , CNot <$> go (d - 1) ]
 
+  shrink c = case c of
+    e0 :>: e1 -> ((e0 :>:) <$> shrink e1)
+              ++ ((:>: e1) <$> shrink e0)
+
+    e0 :==: e1 -> ((e0 :==:) <$> shrink e1)
+               ++ ((:==: e1) <$> shrink e0)
+
+    e0 :&&: e1 -> ((e0 :&&:) <$> shrink e1)
+               ++ ((:&&: e1) <$> shrink e0)
+    
+    CNot c     -> CNot <$> shrink c
+
 instance Arbitrary AtomicStatement where
   arbitrary = oneof [ return Skip
                     , (:=) <$> arbitrary <*> arbitrary
                     , WriteArray <$> arbitrary <*> arbitrary <*> arbitrary ]
+
+  shrink a = case a of
+    Skip               -> []
+    v := e             -> (v :=) <$> shrink e
+    WriteArray v e0 e1 -> (WriteArray v e0 <$> shrink e1)
+                       ++ ((\e -> WriteArray v e e1) <$> shrink e0)
+
+shrinkStmt :: Statement h -> [Statement h]
+shrinkStmt s = case s of
+  SAtom a      -> SAtom <$> shrink a
+  SSeq s0 s1   -> [s0, s1]
+               ++ shrinkStmt s0
+               ++ shrinkStmt s1
+               ++ ((SSeq s0)         <$> shrinkStmt s1)
+               ++ ((\s -> SSeq s s1) <$> shrinkStmt s0)
+  SIf c s0 s1  -> [s0, s1]
+               ++ ((\c -> SIf c s0 s1) <$> shrink c)
+               ++ ((\s -> SIf c s s1) <$> shrinkStmt s0)
+               ++ ((\s -> SIf c s0 s) <$> shrinkStmt s1)
+  SWhile c s   -> [s]
+               ++ ((\c -> SWhile c s) <$> shrink c)
+               ++ ((SWhile c) <$> shrinkStmt s)
 
 instance Arbitrary (Statement NoHole) where
   arbitrary = sized go
@@ -40,6 +82,7 @@ instance Arbitrary (Statement NoHole) where
                    , SSeq <$> go (d `div` 2) <*> go (d `div` 2)
                    , SIf  <$> arbitrary <*> go (d `div` 2) <*> go (d `div` 2)
                    , SWhile <$> arbitrary <*> go (d `div` 2) ]
+  shrink = shrinkStmt
 
 instance Arbitrary (Statement MaybeHole) where
   arbitrary = sized go
@@ -51,6 +94,7 @@ instance Arbitrary (Statement MaybeHole) where
                    , SSeq <$> go (d `div` 2) <*> go (d `div` 2)
                    , SIf  <$> arbitrary <*> go (d `div` 2) <*> go (d `div` 2)
                    , SWhile <$> arbitrary <*> go (d `div` 2) ]
+  shrink = shrinkStmt
 
 prop_id :: Statement NoHole -> Edit -> Bool
 prop_id s delta = apply s delta == Just (s, delta)
