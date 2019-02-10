2254a0ca3f180718ef6214acf2286a15fcab81a5
Maximilian Algehed <m.algehed@gmail.com>
2019-02-10 08:10:47 +0100

    Documentation: Added more examples

--- examples/Examples.hs
+++ examples/Examples.hs
@@ -2,22 +2,32 @@
 module Examples where
 
 import ProgramRep
-import Logic
 import EmbeddedSyntax
+import Logic
 
 import Prelude (($))
-import GHC.Exts
 
 p0 :: Statement
 p0 = do
+  "x" := "y"
+  "y" := 1
+
+p1 :: Statement
+p1 = do
+  "z" := "y"
+  "y" := 1
+  "x" := "z"
+
+p2 :: Statement
+p2 = do
   if "x" :>: "y" & "z" :>: "y" then
     "z" := 6
   else
     "z" := 5
   "z" := "z" + "x"
 
-p1 :: Statement
-p1 = do
+p3 :: Statement
+p3 = do
   "z" := 0
   if "y" :>: "x" then
     while ("y" :>: "x") $ do
