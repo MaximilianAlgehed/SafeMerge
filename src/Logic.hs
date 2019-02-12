11c41562272843d774c4928a569df1f4709d0d92
Maximilian Algehed <m.algehed@gmail.com>
2019-02-12 12:09:26 +0100

    Feature: Added disjunction to the logic

--- src/Logic.hs
+++ src/Logic.hs
@@ -9,6 +9,7 @@ import Data.Data
 
 data Formula where
   (:&)  :: Formula -> Formula -> Formula
+  (:|)  :: Formula -> Formula -> Formula
   (:->) :: Formula -> Formula -> Formula 
   (:=:) :: Expr    -> Expr    -> Formula
   (:>)  :: Expr    -> Expr    -> Formula
@@ -19,7 +20,8 @@ instance Show Formula where
   showsPrec p f = case f of
     e0 :=: e1 -> showString $ show e0 ++ " = " ++ show e1
     e0 :>  e1 -> showString $ show e0 ++ " > " ++ show e1
-    f0 :&  f1 -> showParen (p >= 4) $ showsPrec 3 f0 . showString " & " . showsPrec 3 f1
+    f0 :&  f1 -> showParen (p >= 5) $ showsPrec 5 f0 . showString " & " . showsPrec 4 f1
+    f0 :|  f1 -> showParen (p >= 4) $ showsPrec 4 f0 . showString " | " . showsPrec 3 f1
     FNot f'   -> showParen (p >= 5) $ showString "! " . showsPrec 4 f'
     f0 :-> f1 -> showParen (p >= 3) $ showsPrec 3 f0 . showString " -> " . showsPrec 2 f1
 
